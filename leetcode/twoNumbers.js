867acd7090bb119563ec0f3ed3f1e18a29a0c828
Munna Ahmed <munnaahmed2025@gmail.com>
2023-12-26 23:37:32 +0600

    commit

---
+++ leetcode/twoNumbers.js
@@ -0,0 +1,12 @@
+/*
+    You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+
+    You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+*/
+
+const addTwoNumbers = function (l1, l2) {};
+
+let arr1 = [2, 4, 3];
+let arr2 = [2, 4, 3];
+
+addTwoNumbers(arr1, arr2);
